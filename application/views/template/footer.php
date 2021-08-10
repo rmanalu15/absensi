4be7a94c7c9431689442bbd386f0aec685b7679f
rmanalu15 <manalurupinda8@gmail.com>
2021-08-10 04:15:04 -0700

    Hari Ke-1

---
+++ application/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong> &copy; 2021 <a href="#">Absensi TPQ Al Fatih</a>.</strong> Template by Yayasan Al Fatih
+</footer>
